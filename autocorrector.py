1b51a74f0ba696261f3f3e5ee53857a77bbbce99
Leandro-Resendiz <159493735+Leandro-Resendiz@users.noreply.github.com>
2024-03-09 17:44:30 -0600

    Autocorrect
    Autocorrector Español en python

---
+++ autocorrector.py
@@ -0,0 +1,11 @@
+from autocorrect import Speller
+
+corrector = Speller(lang='es')
+
+def correct_word(word):
+
+    if not corrector(word)== word:
+        return corrector(word)
+    else:
+        return word
+print(correct_word('Me guzta el cholocate y el ejersisio'))
